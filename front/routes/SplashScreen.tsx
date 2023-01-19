a80e600d519d761cb203f43f538a8128b7a7863c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-01-19 10:10:53 +0100

    dev: splascreen css ok

--- front/routes/SplashScreen.tsx
+++ front/routes/SplashScreen.tsx
@@ -3,16 +3,32 @@ import {ActivityIndicator, Image, StyleSheet, Text, View} from 'react-native';
 
 const SplashScreen = () => {
   return (
-    <View>
+    <View style={styles.view}>
       <Image source={require('../assets/photobook.png')} style={styles.image} />
-      <Text>PhotoBook 📸</Text>
-      <ActivityIndicator />
+      <Text style={styles.text}>PhotoBook 😊</Text>
+      <ActivityIndicator
+        style={styles.activityIndicator}
+        size="large"
+        color="#000000"
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   image: {width: 200, height: 200},
+  view: {
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 30,
+  },
+  activityIndicator: {
+    marginTop: 40,
+    transform: [{scaleX: 2}, {scaleY: 2}],
+  },
 });
 
 export default SplashScreen;
