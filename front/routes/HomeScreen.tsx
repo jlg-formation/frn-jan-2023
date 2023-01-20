5908a3f30eed47fc12dbd537c0c89ae893baf00e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-01-20 16:12:54 +0100

    dev: added plugin for camera

--- front/routes/HomeScreen.tsx
+++ front/routes/HomeScreen.tsx
@@ -1,34 +1,52 @@
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import React, {useEffect} from 'react';
-import {Pressable, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {useRecoilValue} from 'recoil';
 import {userState} from '../store/AuthenticationState';
 import {gs} from '../styles';
-import {RootStackParamList} from './navigation';
 
-type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
-
-const HomeScreen = ({navigation}: Props) => {
+const HomeScreen = () => {
   const user = useRecoilValue(userState);
+  const [image, setImage] = useState(undefined as undefined | string);
   useEffect(() => {
     console.log('instantiate home screen');
   }, []);
 
-  const onPress = () => {
-    console.log('about to go to legal page');
-    navigation.navigate('Legal');
+  const onPress = async () => {
+    console.log('about to open photo gallery');
+    const result = await launchImageLibrary({
+      mediaType: 'photo',
+    });
+    console.log('result: ', result);
+    if (result.assets === undefined) {
+      console.log('no assets');
+      return;
+    }
+    for (const asset of result.assets) {
+      console.log('asset: ', asset);
+      setImage(asset.uri);
+    }
   };
 
   return (
     <View style={styles.view}>
       <Text style={[gs.text, styles.text]}>Welcome {user?.displayName}</Text>
-      <Text>
-        <Icon name="home" size={30} color="#900" />
-      </Text>
+
       <Pressable onPress={onPress}>
-        <Text>Go to Legal</Text>
+        <Text>
+          <Icon name="photo-camera" size={60} color="#900" />
+        </Text>
       </Pressable>
+      <Text>{image}</Text>
+      {image && (
+        <Image
+          source={{
+            uri: image,
+          }}
+          style={styles.image}
+        />
+      )}
     </View>
   );
 };
@@ -44,6 +62,7 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: 'bold',
   },
+  image: {width: 200, height: 200},
 });
 
 export default HomeScreen;
